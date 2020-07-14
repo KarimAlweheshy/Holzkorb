2202b6b80d61c8e65bef610fb459a82e1b4db003
Karim <karim.alweheshy@gmail.com>
2020-07-14 12:46:38 +0200

    add heroku configs

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,6 +1,6 @@
-port = 5000;
+port = process.env.PORT || 5000;
 JwtSecret = 'B747865D48A8216B11F23F4E9358B';
-mongoURI = 'mongodb://localhost:27017';
+mongoURI = process.env.MONGODB_URI || 'mongodb://localhost/holzkorb';
 
 module.exports = {
   port,
